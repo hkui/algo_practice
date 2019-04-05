4c6dd8a51355b28d70735e96fd48ba13151cfae9
hk <764432054@qq.com>
2019-04-05 22:35:04 +0800

    代码精简

--- c/linklist/palindrome_str/palindrome_str.c
+++ c/linklist/palindrome_str/palindrome_str.c
@@ -19,35 +19,26 @@ int isPalindrome(strnode *head){
 		prev=slow;
 		slow=next;	
 	}
+
+	strnode *left_begin=prev;
+	strnode *right_begin;
+
 	//奇数个
 	if(quick !=NULL){
-
-		//比如 abcba slow在 c,prev在第1个b
-		strnode *right_begin=slow->next; //舍弃中间元素，从它下一个开始比较
-		strnode *left_begin=prev;
-
-		while(right_begin!=NULL){
-			if(left_begin->c!=right_begin->c){
-				return 0;
-			}
-			left_begin=left_begin->next;
-			right_begin=right_begin->next;
-		}
-
-
+		//比如 abcba slow在 c,prev在第1个b  舍弃中间元素，从它下一个开始比较
+		right_begin=slow->next;
+	}else{
+		//偶数个的 比如abccba,此时prev在第1个c,slow在第二个c
+		right_begin=slow;
 	}
-	//偶数个的 比如abccba,此时prev在第1个c,slow在第二个c
-	else{
-		strnode *left_begin=prev;
-		strnode *right_begin=slow;
-		while(right_begin!=NULL){
-			if(right_begin->c!=left_begin->c){
-				return 0;
-			} 
-			left_begin=left_begin->next;
-			right_begin=right_begin->next;
-		}
+	while(right_begin!=NULL){
+		if(right_begin->c!=left_begin->c){
+			return 0;
+		} 
+		left_begin=left_begin->next;
+		right_begin=right_begin->next;
 	}
+
 	return 1;
 }
 void dump(strnode * head){
