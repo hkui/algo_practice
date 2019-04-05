e7648cbf8266dd6a1356a5afe8bb66e78b75a955
hk <764432054@qq.com>
2019-04-05 19:03:11 +0800

    注释回文

--- c/linklist/palindrome_str/palindrome_str.c
+++ c/linklist/palindrome_str/palindrome_str.c
@@ -1,6 +1,11 @@
 #include "palindrome_str.h"
 
-//判断是否是回文数
+//判断是否是回文
+/*
+使用快慢两个指针找到链表中点，慢指针每次前进一步，快指针每次前进两步。
+在慢指针前进的过程中，同时修改其 next 指针，使得链表前半部分反序。
+最后比较中点两侧的链表是否相等
+*/
 int isPalindrome(strnode *head){
 	strnode *slow,*quick,*prev,*next;
 	slow=head->next;
