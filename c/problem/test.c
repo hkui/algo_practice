c06b112820dcc8c9eb65f8ef3552d9c8729fa0cb
hk <764432054@qq.com>
2019-04-16 23:10:36 +0800

    test

--- c/problem/test.c
+++ c/problem/test.c
@@ -11,9 +11,14 @@ struct Person
 int main(int argc, char const *argv[])
 {
 	struct Person p1={"hk",28,1};
+	printf("p1=%x\n",p1);
+	printf("p1.age=%d\n",p1.age);
+	printf("p1=%x,p1.age=%d\n",p1,p1.age);
+
 	struct Person *p=&p1;
-	printf("p1=%p,p1.age=%d\n",p1 ,p1.age);
+	
 	printf("p=%p\n",p);
+	return 0;
 
 	
 
