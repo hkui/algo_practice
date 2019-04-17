f80c4c06807a059b8e361ea4333ca662e5d50f70
huangkui <huangkui@lepu.cn>
2019-04-17 20:16:27 +0800

    printf

--- c/problem/printf.c
+++ c/problem/printf.c
@@ -2,25 +2,22 @@
 
 struct Person
 {
-	char name[20];
-	int age;
+	
 	int sex;
+	int age;
+	
+	char name[20];
 	
 };
 
 int main(int argc, char const *argv[])
 {
-	struct Person p1={"hk",28,1};
+	struct Person p1={1,28,"hk"};
+	//错误操作
 	printf("p1=%x\n",p1);
 	printf("p1.age=%d\n",p1.age);
 	printf("p1=%x,p1.age=%d\n",p1,p1.age);
-
-	struct Person *p=&p1;
-	
-	printf("p=%p\n",p);
 	return 0;
-
-	
 
 
 }
