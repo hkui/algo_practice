5c60fa18fb2b0b92db8e831b93ae00df96ed6ac6
hk <764432054@qq.com>
2019-06-26 23:29:08 +0800

    环形队列

--- c/queue/circleQueue.c
+++ c/queue/circleQueue.c
@@ -1,4 +1,76 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+//为了避免数据的搬移，使用环形数组队列
+typedef struct {
+    int size,used;
+    int head,tail;
+    int *array;
+} circleQueue;
+
+//创建1个size大小的队列
+circleQueue * createQueueBySize(int size){
+    circleQueue *queue=malloc(sizeof(circleQueue));
+    queue->size=size;
+    queue->used=0;
+    queue->head=0;
+    queue->tail=0;
+    queue->array=(int *)malloc(size* sizeof(int));
+    return queue;
+}
+int enqueue(circleQueue *queue,int val){
+    //判断是不是满了
+    if(queue->used ==queue->size) {
+        return -1;
+    }
+    queue->array[queue->tail]=val;
+    queue->tail=(queue->tail+1)%queue->size;
+    queue->used++;
+}
+//出队
+int dequeue(circleQueue *queue){
+    if(queue->used==0){
+        return -1;
+    }
+    int item=queue->array[queue->head];
+    queue->head=(queue->head+1)%queue->size;
+    queue->used--;
+    return item;
+}
+void dump(circleQueue *queue){
+    if(queue->used==0){
+        return ;
+    }
+    circleQueue *tmpq=(circleQueue *)malloc(sizeof(queue));
+    tmpq=queue;
+    int i=0;
+    while (i<queue->used){
+        int item=tmpq->array[tmpq->head];
+        printf("%d ",item);
+        tmpq->head=(tmpq->head+1)%queue->size;
+        i++;
+    }
+//    free(tmpq);
+    printf("\n###############\n");
+}
+
+
+int main(){
+    circleQueue *queue=createQueueBySize(4);
+    enqueue(queue,2);
+    enqueue(queue,1);
+    enqueue(queue,4);
+    enqueue(queue,3);
+    dump(queue);
+    printf("%d \n",dequeue(queue));
+    printf("%d \n",dequeue(queue));
+    printf("%d \n",dequeue(queue));
+    printf("%d \n",dequeue(queue));
+    printf("%d \n",dequeue(queue));
+
+
+    return 0;
+}
 
 
 
