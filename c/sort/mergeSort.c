8809e4a1a33c9637342ab0abede40653080d16a2
hk <764432054@qq.com>
2019-07-02 01:01:48 +0800

    睡觉

--- c/sort/mergeSort.c
+++ c/sort/mergeSort.c
@@ -1,3 +1,26 @@
 //归并排序
 
+#include <stdio.h>
+//总的归并排序
+void mergeSort(int  arr[],int size){
+    int i=0;
+    int mid=(i+size)%2;
+    mergeSort_(i,mid);
+    mergeSort_(mid+1,size-1);
+    merge();
 
+}
+
+//合并两个有序数组
+merge(){
+
+}
+
+mergeSort_(p,r){
+
+}
+
+int main(){
+
+    return 0;
+}
