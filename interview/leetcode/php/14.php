803937dc6413512118d552b9202620451a3dfe45
hk <764432054@qq.com>
2019-11-22 23:01:49 +0800

    最长公共前缀zc

--- interview/leetcode/php/14.php
+++ interview/leetcode/php/14.php
@@ -1,7 +1,29 @@
 <?php
 
+var_dump(stripos("flight", "flo"));
+
+
 function longestCommonPrefix($strs) {
-    
+   $prefix='';	
+   $first=$strs[0];
+   $first_len=strlen($first);
+
+   $strs_num=count($strs);
+   for($i=0;$i<$first_len;$i++){
+   		$tmp=$prefix.$first[$i];
+
+   		for($j=1;$j<$strs_num;$j++){
+   			if(stripos($strs[$j], $tmp)!==0){
+  				return $prefix;
+   				
+   			}
+   		}
+   		$prefix=$tmp;
+   }
+   return $prefix;
 }
+
+$strs=["flower","flow","flight"];
+echo longestCommonPrefix($strs);
 
 ?>
