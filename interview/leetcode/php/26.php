641294ab511185d3cd736412cb67f451495020aa
hk <764432054@qq.com>
2019-11-25 23:08:15 +0800

    zc

--- interview/leetcode/php/26.php
+++ interview/leetcode/php/26.php
@@ -1,9 +1,42 @@
 <?php
 //删除排序数组中的重复项 
 
-function removeDuplicates(&$nums) {
-     $nums=array_unique($nums);
-     return count($nums);   
+function removeDuplicates1(&$nums) {
+     $len=count($nums);
+     for($i=0;$i<$len-1;$i++){
+     	while (true) {
+     		if($nums[$i+1] ==$nums[$i]){
+	     		for($j=$i+1;$j<$len-1;$j++){
+	     			$nums[$j]=$nums[$j+1];
+	     		}
+	     		$len--;
+     		}else{
+     			break;
+     		}
+     	}
+     	
+     }
+     return $len;
 }
 
+function removeDuplicates(&$nums) {
+     $len=count($nums);
+     $i=0;
+     while($i<$len-1){
+     	if($nums[$i+1] ==$nums[$i]){
+     		for($j=$i;$j<$len-1;$j++){
+     			$nums[$j]=$nums[$j+1];
+     		}
+     		$len--;
+ 		}else{
+ 			$i++;
+ 		}
+     }
+     
+     return $len;
+}
+
+$test=[0,0,1,1,1,2,2,3,3,4];
+$r=removeDuplicates($test);
+print_r($test);
 ?>
