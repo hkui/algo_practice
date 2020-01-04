f18d46906e49ae83019492fc9465815de04288ea
hk <764432054@qq.com>
2020-01-04 23:18:46 +0800

    删除链表的倒数第n个节点

---
+++ interview/leetcode/php/19.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * 删除链表的倒数第N个节点
+ *  https://leetcode-cn.com/problems/remove-nth-node-from-end-of-list/
+ * */
+
+
+
+class Solution
+{
+
+    /**
+     * @param ListNode $head
+     * @param Integer $n
+     * @return ListNode
+     */
+    function removeNthFromEnd($head, $n)
+    {
+        $fast=$slow=$head;
+        for($i=0;$i<$n;$i++){
+            $fast=$fast->next;
+        }
+        $prev=null;
+        while($fast){
+            $fast=$fast->next;
+            $prev=$slow;
+            $slow=$slow->next;
+        }
+        if(empty($prev)){
+            return $head->next;
+        }
+        $prev->next=$slow->next;
+        return $head;
+    }
+}
+
+class ListNode
+{
+    public $val = 0;
+    public $next = null;
+
+    function __construct($val)
+    {
+        $this->val = $val;
+    }
+}
+
+function createNodeList($arr){
+    $head=new ListNode(null);
+
+    $cur=$head;
+    foreach ($arr as $v){
+        $node=new ListNode($v);
+        $cur->next=$node;
+        $cur=$cur->next;
+
+
+    }
+    return $head->next;
+}
+$arr=["a","b","c","d",'e','f','g','h'];
+$nodeList=createNodeList($arr);
+
+$so=new Solution();
+$r=$so->removeNthFromEnd($nodeList,7);
+print_r($r);
