925773afccc17dc7b4e55d5e1f297f00f5c93333
hk <764432054@qq.com>
2020-01-12 11:51:47 +0800

    寻找两个有序数组的中位数

--- interview/leetcode/php/04.php
+++ interview/leetcode/php/04.php
@@ -2,7 +2,6 @@
 
 class Solution {
 
-    public $debug=0;
     /**
      * @param Integer[] $nums1
      * @param Integer[] $nums2
@@ -11,17 +10,19 @@ class Solution {
     function findMedianSortedArrays($nums1, $nums2) {
         $len1=count($nums1);
         $len2=count($nums2);
-        $k=intval(($len1+$len2)/2);
+        $sum=$len1+$len2;
+        $k=ceil(($sum)/2);
+        $leftmid=$this->getKth($nums1,0,$len1-1,$nums2,0,$len2-1,$k);
 
-        return $this->getKth($nums1,0,$len1-1,$nums2,0,$len2-1,$k);
-
-
-
+        if($sum%2!=0){
+            return $leftmid;
+        }
+        $rightmid=$this->getKth($nums1,0,$len1-1,$nums2,0,$len2-1,$k+1);
+        return ($rightmid+$leftmid)*0.5;
 
     }
 
     function getKth($nums1,$start1,$end1,$nums2,$start2,$end2,$k){
-        $this->debug++;
         $len1=$end1-$start1+1;
         $len2=$end2-$start2+1;
         if($len1>$len2){
@@ -48,6 +49,8 @@ $so=new Solution();
 
 $tests=[
     [[1,2,6],[0,3,4,5]],
+    [[1,3],[2,4]],
+    [[1,3],[2]],
 
 ];
 foreach ($tests as $t){
