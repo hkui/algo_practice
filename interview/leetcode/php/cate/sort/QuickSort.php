512260c84653e56ac046bc2bbd2f580680374f5c
hk <764432054@qq.com>
2020-06-04 00:52:35 +0800

    quickSort

---
+++ interview/leetcode/php/cate/sort/QuickSort.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 764432054@qq.com
+ * Date: 2020/6/3
+ * Time: 22:58
+ * 复习快排
+ */
+
+/**
+ * @param $arr
+ * @param $p    起始区间位置
+ * @param $r    结束区间位置
+ * @return mixed
+ */
+function partition(&$arr,$p,$r){
+    $privot=$arr[$r];
+    $i=$p;
+    //[p,i-1] i [i+1,r]
+
+    for($j=$p;$j<$r;$j++){
+        if($arr[$j]<$privot){
+            $tmp=$arr[$i];
+            $arr[$i]=$arr[$j];
+            $arr[$j]=$tmp;
+            $i++;
+        }
+    }
+
+    $arr[$r]=$arr[$i];
+    $arr[$i]=$privot;
+    return $i;
+}
+
+/**
+ * @param $arr
+ * @param $p 开始排序的起始位置(包括)
+ * @param $r 开始排序的结束位置(包括)
+ */
+
+function quickSort(&$arr,$p,$r){
+    if($p>=$r){
+        return ;
+    }
+    $i=partition($arr,$p,$r);
+    quickSort($arr,$p,$i-1);
+    quickSort($arr,$i+1,$r);
+}
+
+
+
+
+
+$arr= [4,2,5,1,6,3];
+//$arr=[1,2,3,4,5,6];
+//$arr=[6,5,4,3,1,2];
+
+
+quickSort($arr,0,count($arr)-1);
+print_r($arr);
