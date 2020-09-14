ecabf35ac587445bd8b548873ecda125ea67c3bd
hkui <764432054@qq.com>
2020-09-14 19:34:42 +0800

    dp53

--- leetcode/php/053.php
+++ leetcode/php/053.php
@@ -25,6 +25,26 @@ function maxSubArray($nums) {
     }
     return $max;
 }
+
+/**
+ *动态规划
+ */
+function maxSubArrayDp($nums){
+    $len=count($nums);
+    if($len<1){
+        return 0;
+    }
+
+    $dp[0]=$nums[0];
+    $max=$dp[0];
+
+    for($i=1;$i<$len;$i++){
+        $dp[$i]=$dp[$i-1]>0?($nums[$i]+$dp[$i-1]):$nums[$i];
+        $max=max($max,$dp[$i]);
+    }
+    return $max;
+}
+
 $nums=[-2,1,-3,4,-1,2,1,-5,4];
-$nums=[1];
-echo maxSubArray($nums);
+//$nums=[1];
+echo maxSubArrayDp($nums);
