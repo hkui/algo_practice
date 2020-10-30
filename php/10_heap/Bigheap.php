45e8fc655d0815b7515be803c6c10522e657804e
hkui <764432054@qq.com>
2020-10-30 21:13:39 +0800

    zc

--- php/10_heap/Bigheap.php
+++ php/10_heap/Bigheap.php
@@ -18,14 +18,21 @@ class Bigheap
     {
         $this->size=$size;
     }
+
+    /**
+     * @param $data
+     * 插入到数据末尾
+     */
     public function insert($data){
         $this->dataArr[$this->count+1]=$data;
         $this->count++;
-        $this->heap();
-
-
+        $this->heapFromLast();
     }
-    public function heap(){
+
+    /**
+     * 从最后1个起开始堆化
+     */
+    public function heapFromLast(){
         $i=$this->count;
         while( ($p=intval($i/2))>0 && $this->dataArr[$p]<$this->dataArr[$i] ){
             $tmp=$this->dataArr[$p];
@@ -33,8 +40,48 @@ class Bigheap
             $this->dataArr[$i]=$tmp;
             $i=$p;
         }
-
     }
+
+    /**
+     * @return bool
+     * 弹出堆顶
+     * 把最后1个元素放入到堆顶
+     * 从堆顶开始向下堆化
+     */
+    public function pop(){
+        if($this->count<1){
+            return false;
+        }
+        $top=$this->dataArr[1];
+        $this->dataArr[1]=$this->dataArr[$this->count];
+        $this->count--;
+        $this->heapFromTop();
+        return $top;
+    }
+    public function heapFromTop(){
+        $i=1;
+        while(($left=2*$i) && $left<=$this->count){
+            if($left==$this->count){
+               if($this->dataArr[$left]>$this->dataArr[$i]){
+                    $tmp=$this->dataArr[$i];
+                    $this->dataArr[$i]=$this->dataArr[$left];
+                    $this->dataArr[$left]=$tmp;
+                    break;
+               }
+            }else{
+                $right=$left+1;
+                $maxPos=$this->dataArr[$left]>$this->dataArr[$right]?$left:$right;
+                if($this->dataArr[$maxPos]>$this->dataArr[$i]){
+                    $tmp=$this->dataArr[$maxPos];
+                    $this->dataArr[$maxPos]=$this->dataArr[$i];
+                    $this->dataArr[$i]=$tmp;
+                }
+                $i=$maxPos;
+            }
+
+        }
+    }
+
 
 }
 $bigHeap=new Bigheap(0);
@@ -44,3 +91,7 @@ $bigHeap->insert(6);
 $bigHeap->insert(5);
 $bigHeap->insert(7);
 print_r($bigHeap->dataArr);
+var_dump($bigHeap->pop());
+var_dump($bigHeap->pop());
+var_dump($bigHeap->pop());
+
