75a817c9a87fa782c31450131d1a0434a7e86763
hkui <764432054@qq.com>
2020-11-21 16:57:25 +0800

    test挪到1个文件里

--- leetcode/go/test/027_test.go
+++ leetcode/go/test/027_test.go
@@ -7,11 +7,7 @@ import (
 )
 
 func TestRemoveElement(t *testing.T)  {
-
-
 		res:=algo.RemoveElement([]int{1,2,3,4,2,5},2)
 		fmt.Println(res)
-
-
 }
 
--- leetcode/go/test/algo_test.go
+++ leetcode/go/test/algo_test.go
@@ -9,6 +9,115 @@ import (
 var res [][]int
 type intArr []int
 
+func Test001TwoSum(t *testing.T)  {
+	nums:=[]int{2,7,8,0,11,15}
+	target:=15
+	res:=algo.TwoSum(nums,target)
+	fmt.Printf("res=%+v\n",res)
+
+}
+func Test002AddTwoNumbers(t *testing.T)  {
+	var l1,l2 *algo.ListNode
+	l1=algo.CreateNodeList([]int{2,4,3})
+	l2=algo.CreateNodeList([]int{5,6,4})
+
+	l1=algo.CreateNodeList([]int{1})
+	l2=algo.CreateNodeList([]int{9,9})
+
+	res:=algo.AddTwoNumbers(l1,l2)
+	algo.PrintNode(res)
+}
+
+func Test003LengthOfLongestSubstring(t *testing.T)  {
+
+	tests:=[]string{
+		" ",
+		//"abcabcbb",
+		//"bbbbb",
+		//"pwwkew",
+		//"abba",
+
+
+	}
+	for _,s:=range tests{
+		res:=algo.LengthOfLongestSubstring(s)
+		fmt.Println(res)
+	}
+
+}
+func Test006Convert(t *testing.T)  {
+	s:="abcdefghijk"
+	s="PAYPALISHIRING"
+
+	res:=algo.Convert(s,4)
+	fmt.Println(res)
+
+}
+func Test007Reverse(t *testing.T)  {
+	rev:=algo.Reverse(2147483648)
+	fmt.Println(rev)
+
+}
+func Test009IsPalindrome(t *testing.T)  {
+	tests:=[]int{
+		123,
+		121,
+		1221,
+	}
+	for _,i:=range tests{
+		r:=algo.IsPalindrome(i)
+		fmt.Println(i,r)
+	}
+
+}
+func Test011MaxArea(t *testing.T)  {
+	heights:=[]int{1,8,6,2,5,4,8,3,7}
+	res:=algo.MaxArea(heights)
+	fmt.Println(res)
+}
+func Test014LongestCommonPrefix(t *testing.T)  {
+	tests:=[][]string{
+		{"flower","flow","flight"},
+		{"cd","ab"},
+	}
+	for _,v:=range tests{
+		res:=algo.LongestCommonPrefix(v)
+		fmt.Println(res)
+	}
+}
+func Test015ThreeSum(t *testing.T)  {
+	test:=[]int{-1, 0, 1, 2, -1, -4}
+	res:=algo.ThreeSum(test)
+	fmt.Println(res)
+}
+func Test019RemoveNthFromEnd(t *testing.T)  {
+
+	head:=algo.CreateNodeList([]int{1,2,3,4,5,6})
+
+	res:=algo.RemoveNthFromEnd(head,4)
+	algo.PrintNode(res)
+
+}
+func Test020IsValid(t *testing.T)  {
+	tests:=[]string{
+		"{}",
+	}
+	for _,s:=range tests{
+		res:=algo.IsValid(s)
+		fmt.Println(res)
+	}
+
+}
+func Test021MergeTwoLists(t *testing.T)  {
+
+	res:=algo.MergeTwoLists(algo.CreateNodeList([]int{1,3,5}),algo.CreateNodeList([]int{2,4}))
+	algo.PrintNode(res)
+}
+func Test027RemoveElement(t *testing.T)  {
+	res:=algo.RemoveElement([]int{1,2,3,4,2,5},2)
+	fmt.Println(res)
+}
+
 
 func Test46Permute(t *testing.T)  {
 
