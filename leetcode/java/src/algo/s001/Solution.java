25dd2cb314b2f28cf0e19d471928997c5b827d85
hkui <764432054@qq.com>
2021-10-08 00:39:57 +0800

    java001

---
+++ leetcode/java/src/algo/s001/Solution.java
@@ -0,0 +1,32 @@
+package algo.s001;
+
+import java.util.*;
+
+class Solution {
+
+
+    public static void main(String[] args) {
+//       int []nums=new int[]{2,7,11,15};
+//       int target=9;
+
+        int []nums=new int[]{3,3};
+        int target=6;
+       int []ret=twoSum(nums,target);
+        System.out.println(ret[0]);
+
+    }
+    public static int[] twoSum(int[] nums, int target) {
+        Hashtable<Integer,Integer> tb=new Hashtable<>();
+        int[]ret=new int[2];
+        int len=nums.length;
+        for(int k=0;k<len;k++){
+            int v=nums[k];
+            tb.put(target-v,k);
+            if(tb.containsKey(v)){
+                ret[0]=tb.get(v);
+                ret[1]=k;
+            }
+        }
+        return ret;
+    }
+}
